6da9c439e2b809aa0104640873b86f2b2b72fc61
christian-lopezxd <00179320@uca.edu.sv>
2021-11-26 18:15:27 -0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import NavBar from './Componentes/UserView/Components/NavBar/NavBar';
+import GetAll from './Componentes/UserView/Components/GetAll/GetAll';
+import getallfavorites from './Componentes/UserView/Components/GetAll/GetAllFavorites';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path='/' exact component={GetAll} />
+          <Route path='/' component={getallfavorites} />
+
+        </Switch>
+      </Router>
+
+
+
     </div>
   );
 }
---
+++ src/Componentes/UserView/Components/GetAll/GetAllFavorites.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const getallfavorites = () => {
+  return (
+    <div>
+      <h1>GetAll Favorites</h1>
+    </div>
+  );
+};
+
+export default getallfavorites;
